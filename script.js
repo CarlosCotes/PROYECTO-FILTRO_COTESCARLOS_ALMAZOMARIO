5350714118245164e7df6d807ceba4c4019c60c0
CarlosCotes <kmayo2013@gmail.com>
2024-08-20 17:01:27 -0500

    fix: :zap:

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function addToCart(product) {
+  var cart = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  cart.push(product);
+
+  localStorage.setItem("carrito", JSON.stringify(cart));
+
+  alert("Producto añadido al carrito");
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   fetch("/productos.json")
     .then((response) => response.json())
@@ -16,14 +26,49 @@ document.addEventListener("DOMContentLoaded", function () {
                 <div class="content">
                     <div class="precio">${product.precio}</div>
                     <div class="comprar">
-                         <button class="btncr" id='${product.img + ';' +product.referencia + ';' +product.precio+ ';' +product.tipo}'>Agregar al carrito</button>
+                    <button class="btncr" data-product='${JSON.stringify(product)}'>Agregar al carrito</button>
                 </div>
             </div>
         `;
 
         cardContainer.appendChild(card);
       });
-
+      document.querySelectorAll(".btncr").forEach(button => {
+        button.addEventListener("click", function () {
+            const product = JSON.parse(this.getAttribute('data-product'));
+            addToCart(product); 
+          });
     })
     .catch((error) => console.error("Error al cargar los productos:", error));
+})
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const containercard = document.getElementById('container-cart');
+  const cart = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  if (cart.length === 0) {
+      containercard.innerHTML = "<h1>Tu carrito está vacío.<h1>";
+      return;
+  }
+
+cart.forEach(product , function (){
+      const cart = document.createElement('div');
+      cart.className = 'cart';
+
+      cart.innerHTML = `
+      <div class="box box1">
+              <img src="${product.img}" alt="${product.alt}">
+              <div class="referencia">${product.referencia}</div>
+      </div>
+      <div class="box box2">
+          <div class="content">
+              <div class="precio">${product.precio}</div>
+              <div class="comprar">
+              <button class="btncr" data-product='${JSON.stringify(product)}'>Agregar al carrito</button>
+          </div>
+      </div>
+  `;
+      containercard.appendChild(cart);
+  });
 });
